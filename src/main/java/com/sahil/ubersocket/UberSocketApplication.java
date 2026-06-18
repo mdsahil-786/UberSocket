14432fd50dbb54794fe91c0c85021212785c9e27
sahil <sahil9955555@gmail.com>
2026-06-18 21:37:48 +0530

    All other files are added

---
+++ src/main/java/com/sahil/ubersocket/UberSocketApplication.java
@@ -0,0 +1,13 @@
+package com.sahil.ubersocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sahil/ubersocket/UberSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.ubersocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
