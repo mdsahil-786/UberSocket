ac094ab0d29a559640aabf8e497963dd02addb6a
sahil <sahil9955555@gmail.com>
2026-06-18 17:52:09 +0530

    Controller layer is added

---
+++ src/main/java/com/sahil/ubersocket/controllers/SocketController.java
@@ -0,0 +1,40 @@
+package com.sahil.ubersocket.controllers;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import com.sahil.ubersocket.client.GrpcClient;
+import com.sahil.ubersocket.dtos.RideAcceptanceDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class SocketController {
+
+    private final GrpcClient grpcClient;
+
+
+    @MessageMapping("/ride-acceptance")
+    public void receiveRideAcceptance(RideAcceptanceDTO rideAcceptanceDTO) {
+
+        try {
+            boolean success = grpcClient.acceptRide(rideAcceptanceDTO.getDriverId(), rideAcceptanceDTO.getBookingId());
+            if (success) {
+                log.info("Ride acceptance successful: driverId={}, bookingId={}",
+                        rideAcceptanceDTO.getDriverId(), rideAcceptanceDTO.getBookingId());
+            } else {
+                log.error("Ride acceptance failed: driverId={}, bookingId={}",
+                        rideAcceptanceDTO.getDriverId(), rideAcceptanceDTO.getBookingId());
+            }
+        } catch (Exception e) {
+            log.error("Error processing ride acceptance: driverId={}, bookingId={}",
+                    rideAcceptanceDTO.getDriverId(), rideAcceptanceDTO.getBookingId(), e);
+        }
+
+    }
+}
+
+// ubersocket -- grpc ---> uber booking service
