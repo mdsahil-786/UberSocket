6afff51a65d0597aa1dc9a2663aa078a83392dcf
sahil <sahil9955555@gmail.com>
2026-06-18 18:35:26 +0530

    Config layer is added

---
+++ src/main/java/com/sahil/ubersocket/config/GrpcServerConfig.java
@@ -0,0 +1,57 @@
+package com.sahil.ubersocket.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.sahil.ubersocket.RideNotificationServiceGrpc;
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import io.grpc.Server;
+import jakarta.annotation.PostConstruct;
+import io.grpc.ServerBuilder;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class GrpcServerConfig {
+
+    @Value("${grpc.server.port:9091}")
+    private int grpcServerPort;
+
+    private final RideNotificationServiceGrpc.RideNotificationServiceImplBase rideNotificationServiceImpl;
+    private Server server;
+
+    @PostConstruct
+    public void startGrpcServer() throws IOException {
+        server = ServerBuilder
+                .forPort(grpcServerPort)
+                .addService(rideNotificationServiceImpl)
+                .build()
+                .start();
+
+        System.out.println("gRPC Server started on port " + grpcServerPort);
+
+
+        new Thread(() -> {
+            try {
+                if( server != null ) {
+                    server.awaitTermination();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("gRPC Server interrupted");
+            }
+        }).start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down gRPC Server...");
+            if( server != null ) {
+                server.shutdown();
+            }
+        }));
+
+    }
+}
